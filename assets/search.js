a94bb5b451e66a1622e54f912da403853d36c447
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-02-06 19:13:00 -0800

    Update search.js

--- assets/search.js
+++ assets/search.js
@@ -1,13 +1,14 @@
-// Add event listener
-document.getElementById('search').addEventListener('change', search);
+var input, filter, table, tr, td, i, txtValue;
+input = document.getElementById("myInput");
+filter = input.value.toUpperCase();
+table = document.getElementById("myTable");
+tr = table.getElementsByTagName("tr");
+
+
 
 function search() {
     // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
+
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
@@ -22,3 +23,6 @@ function search() {
       }
     }
   }
+
+  // Add event listener
+document.getElementById('search').addEventListener('change', search);
